5086d2fd712f345046cdbc9c60566c78d0003b43
Mark Featherston <mark@embeddedarm.com>
2016-12-13 15:11:37 -0700

    tsmicroctl: added ts4100 sleep mode

--- src/tsmicroctl.c
+++ src/tsmicroctl.c
@@ -24,8 +24,8 @@ char *get_model()
 
 	proc = fopen("/proc/device-tree/model", "r");
 	if (!proc) {
-	    perror("model");
-	    return 0;
+		perror("model");
+		return 0;
 	}
 	sz = fread(mdl, 256, 1, proc);
 	ptr = strstr(mdl, "TS-");
@@ -37,8 +37,8 @@ int silabs_init()
 	static int fd = -1;
 	fd = open("/dev/i2c-0", O_RDWR);
 	if(fd != -1) {
-		if (ioctl(fd, I2C_SLAVE_FORCE, 0x10) < 0) {
-			perror("FPGA did not ACK 0x10\n");
+		if (ioctl(fd, I2C_SLAVE_FORCE, 0x4a) < 0) {
+			perror("FPGA did not ACK 0x4a\n");
 			return -1;
 		}
 	}
@@ -52,16 +52,16 @@ uint16_t* sread(int twifd, uint16_t *data)
 	bzero(tmp, 30);
 	int i;
 
-    read(twifd, tmp, 30);
-    for (i = 0; i < 15; i++)
-    	data[i] = (tmp[i*2] << 8) | tmp[(i*2)+1];
+	read(twifd, tmp, 30);
+	for (i = 0; i < 15; i++)
+		data[i] = (tmp[i*2] << 8) | tmp[(i*2)+1];
 
 	return data;
 }
 
 // Scale voltage to silabs 0-2.5V
 uint16_t inline sscale(uint16_t data){
-	return data * 2.5 * 1024/1000;
+	return data * (2.5/1023) * 1000;
 }
 
 // Scale voltage for resistor dividers
@@ -71,40 +71,51 @@ uint16_t inline rscale(uint16_t data, uint16_t r1, uint16_t r2)
 	return sscale(ret);
 }
 
-#ifdef CTL
+void do_sleep(int twifd, int seconds)
+{
+	unsigned char dat[4] = {0};
+	int opt_sleepmode = 1; // Legacy mode on new boards
+	int opt_resetswitchwkup = 1;
+
+	dat[0]=(0x1 | (opt_resetswitchwkup << 1) |
+	  ((opt_sleepmode-1) << 4) | 1 << 6);
+	dat[3] = (seconds & 0xff);
+	dat[2] = ((seconds >> 8) & 0xff);
+	dat[1] = ((seconds >> 16) & 0xff);
+	write(twifd, &dat, 4);
+}
 
 void do_info(int twifd)
 {
-	uint16_t data[20];
+	uint16_t data[15];
 	memset(data, 0, sizeof(uint16_t));
 	sread(twifd, data);
 
-	if(strstr(model, "7970")) {
-		printf("VDD_ARM_CAP=%d\n", sscale(data[0]));
-		printf("VDD_HIGH_CAP=%d\n", sscale(data[1]));
-		printf("VDD_SOC_CAP=%d\n",sscale(data[2]));
-		printf("VDD_ARM=%d\n", sscale(data[3]));
-		printf("SILAB_P10=0x%X\n", data[4]);
-		printf("SILAB_P11=0x%X\n", data[5]);
-		printf("SILAB_P12=0x%X\n", data[6]);
-		printf("VIN=%d\n", rscale(data[7], 2870, 147));
-		printf("V5_A=%d\n", rscale(data[8], 147, 107));
-		printf("V3P1=%d\n", rscale(data[9], 499, 499));
-		printf("DDR_1P5V=%d\n", sscale(data[10]));
-		printf("V1P8=%d\n", sscale(data[11]));
-		printf("V1P2=%d\n", sscale(data[12]));
-		printf("RAM_VREF=%d\n", sscale(data[13]));
-		printf("V3P3=%d\n", sscale(data[14]));
+	if(strstr(model, "4100")) {
+		/* Byte order is P1.2-P1.4, P2.0-P2.7, temp sensor */
+		printf("AN_SUP_CAP_1=%d\n", sscale(data[0]));
+		printf("AN_SUP_CAP_2=%d\n", rscale(data[1], 20, 20));
+		printf("AN_MAIN_4P7V=%d\n", rscale(data[2], 20, 20));
+		printf("MAIN_5V=%d\n", rscale(data[3], 536, 422));
+		printf("USB_OTG_5V=%d\n", rscale(data[4], 536, 422));
+		printf("V3P3=%d\n", rscale(data[5], 422, 422));
+		printf("RAM_1P35V=%d\n", sscale(data[6]));
+		printf("VDD_6UL_CORE=%d\n", sscale(data[9]));
+		printf("AN_CHRG=%d\n", rscale(data[10], 422, 422));
+		printf("VDD_SOC_CAP=%d\n", sscale(data[11]));
+		printf("VDD_ARM_CAP=%d\n", sscale(data[12]));
+		//printf("SILAB_REV=%d\n", data[)
 	}
 }
 
 static void usage(char **argv) {
 	fprintf(stderr, "Usage: %s [OPTION] ...\n"
 	  "Technologic Systems Microcontroller Access\n"
-	  "\n"
+	  "\n"	
 	  "  -h, --help            This message\n"
 	  "  -i, --info            Read all Silabs ADC values\n"
-	  "    All values are returned in mV except the P10 which are raw 12-bit values.\n\n",
+	  "  -s, --sleep <seconds> Put the board in a sleep mode for n seconds\n"
+	  "    All values are returned in mV\n\n",
 	  argv[0]
 	);
 }
@@ -116,6 +127,7 @@ int main(int argc, char **argv)
 
 	static struct option long_options[] = {
 	  { "info", 0, 0, 'i' },
+	  { "sleep", 1, 0, 's' },
 	  { "help", 0, 0, 'h' },
 	  { 0, 0, 0, 0 }
 	};
@@ -126,7 +138,7 @@ int main(int argc, char **argv)
 	}
 
 	model = get_model();
-	if(!strstr(model, "7970")) {
+	if(!strstr(model, "4100")) {
 		fprintf(stderr, "Not supported on model \"%s\"\n", model);
 		return 1;
 	}
@@ -135,10 +147,13 @@ int main(int argc, char **argv)
 	if(twifd == -1)
 		return 1;
 
-	while((c = getopt_long(argc, argv, "ih", long_options, NULL)) != -1) {
+	while((c = getopt_long(argc, argv, "is:h", long_options, NULL)) != -1) {
 		switch (c) {
 		case 'i':
 			do_info(twifd);
+			break;
+		case 's':
+			do_sleep(twifd, atoi(optarg));
 			break;
 		case 'h':
 		default:
@@ -148,5 +163,3 @@ int main(int argc, char **argv)
 
 	return 0;
 }
-
-#endif
