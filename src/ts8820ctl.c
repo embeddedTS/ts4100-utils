6a97a39af30dd04416aa78ca7f2ecda2be1b2044
Kris Bahnsen <kris@embeddedarm.com>
2019-07-19 16:25:54 -0700

    ts8820ctl: Cleaned up ADC operations
    * Add --range and --os to specify input V range/oversample rate
    * Range is passed to ts8820 API to accuractly calc. the read V
    * Range and oversample select GPIOs are using libgpiod
    * Cleaned up help, added comments where appropriate

--- src/ts8820ctl.c
+++ src/ts8820ctl.c
@@ -3,14 +3,16 @@
 /* Based on ts8820ctl.c for the TS-4700 */
 
 #include <assert.h>
+#include <gpiod.h>
 #include <fcntl.h>
-#include <sys/mman.h>
+#include <getopt.h>
+#include <gpiod.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <getopt.h>
-#include <stdint.h>
 #include <string.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #include "ts8820.h"
 #include "fpga.h"
@@ -53,15 +55,21 @@ static void usage(char **argv) {
 	  "Technologic Systems TS-8820-4100 FPGA manipulation\n"
 	  "\n"
 	  " General Options:\n"
-	  "  -s, --sample=<num>     Display num samples per ADC channel in mV\n"
-	  "  -a, --acquire=<num>    Send num raw samples per ADC channel to "\
-	    "stdout\n"
-	  "  -r, --rate=<speed>     Sample at <speed> Hz (default 10000)\n"
-	  "  -m, --mask=<mask>      Sample only channels set in 16-bit <mask>\n"
 	  "  -R, --read             Read 16-bit register at <addr>\n"
 	  "  -W, --write=<val>      Write 16-bit <val> to register at <addr>\n"
 	  "  -A, --address=<addr>   TS-8820 FPGA address to read or write\n"
 	  "  -h, --help             This help\n\n"
+
+	  " ADC Options:\n"
+	  "  -s, --sample=<num>     Print <num> samples per ADC channel in mV\n"
+	  "  -a, --acquire=<num>    Send num raw samples per ADC channel to "\
+	    "stdout\n"
+	  "  -r, --rate=<speed>     Sample at <speed> Hz (default 10000)\n"
+	  "  -m, --mask=<mask>      Sample only channels set in 16-bit <mask>\n"
+	  "  -n, --range=<range>    ADC voltage input range\n"
+	  "  -o, --os=<rate>        Oversample rate (2^<rate>)\n\n"
+	  "  Input <range> is 0 (def.) for -5 V to +5 V. 1 for -10 V to +10 V\n"
+	  "  Oversample rate must be a value 1 through 6 (def.)\n\n"
 
 	  " PWM Options:\n"
 	  "  -p, --pwm=<out>        Set PWM on digital <out> (1-6)\n"
@@ -103,30 +111,44 @@ static void usage(char **argv) {
 }
 
 int main(int argc, char **argv) {
+	/* General */
 	int c;
 	int model;
+	int opt_address = -1, opt_read = 0, opt_write = 0, opt_writearg = 0;
+	/* ADC specific */
 	int opt_sample = 0, opt_acquire = 0;
 	int opt_rate = 10000, opt_mask = 0xffff;
+	int opt_range = 0, opt_oversample = 1;
+	struct gpiod_chip *chip;
+	struct gpiod_line *line22_range, *line23_range;
+	struct gpiod_line *line24_os, *line25_os, *line26_os;
 	/* PWM specific */
 	int opt_pwm = 0, opt_pwmduty = 0, opt_prescaler = 0;
 	/* DAC specific */
 	int opt_setdac = 0, opt_mvolts = 0;
 	/* H Bridge specific */
 	int opt_hb = 0, opt_hbset = -1, opt_hbduty = 0;
+	/* IO specific */
 	int opt_DO = 0, opt_DOarg = 0, opt_DI = 0;
 	int opt_counter = 0, opt_counterarg = 0;
-	int opt_address = -1, opt_read = 0, opt_write = 0, opt_writearg = 0;
+
 	static struct option long_options[] = {
+	  /* ADC opts */
 	  { "sample",	required_argument,	0, 's' },
 	  { "acquire",	required_argument,	0, 'a' },
 	  { "rate",	required_argument,	0, 'r' },
 	  { "mask",	required_argument,	0, 'm' },
+	  { "range",	required_argument,	0, 'n' },
+	  { "os",	required_argument,	0, 'o' },
+	  /* DAC opts */
 	  { "setdac",	required_argument,	0, 'd' },
 	  { "mvolts",	required_argument,	0, 'v' },
+	  /* PWM opts */
 	  { "pwm",	required_argument,	0, 'p' },
 	  { "duty",	required_argument,	0, 'u' },
 	  { "prescaler",required_argument,	0, 'P' },
 	  { "prescalar",required_argument,	0, 'P' },
+	  /* H Bridge opts */
 	  { "hbridge1",	no_argument,		0, '1' },
 	  { "hbridge2",	no_argument,		0, '2' },
 	  { "disable",	no_argument,		0, 'I' },
@@ -134,9 +156,11 @@ int main(int argc, char **argv) {
 	  { "brake",	no_argument,		0, 'B' },
 	  { "fwd",	required_argument,	0, 'F' },
 	  { "rev",	required_argument,	0, 'E' },
+	  /* IO opts */
 	  { "counter",	required_argument,	0, 'c' },
 	  { "setdio",	required_argument,	0, 'D' },
 	  { "getdio",	no_argument,		0, 'G' },
+	  /* General opts */
 	  { "read",	no_argument,		0, 'R' },
 	  { "write",	required_argument,	0, 'W' },
 	  { "address",	required_argument,	0, 'A' },
@@ -150,83 +174,90 @@ int main(int argc, char **argv) {
 	}
 
 	while((c = getopt_long(argc, argv,
-	  "c:p:u:P:12ICBF:E:r:v:m:hs:a:d:D:GRW:A:", long_options, NULL)) != -1) {
+	  "c:p:u:P:12ICBF:E:r:v:m:n:o:hs:a:d:D:GRW:A:",
+	  long_options, NULL)) != -1) {
 		switch (c) {
-		  case 'r':
+		  case 'r': /* ADC sample rate */
 			opt_rate = strtoul(optarg, NULL, 0);
 			break;
-		  case 'm':
+		  case 'm': /* ADC Acquire mask */
 			opt_mask = strtoul(optarg, NULL, 0);
 			break;
-		  case 's':
+		  case 's': /* ADC number of samples */
                         opt_sample = strtoul(optarg, NULL, 0);
 			break;
-		  case 'a':
+		  case 'a': /* ADC number of acquisitions */
 			opt_acquire = strtoul(optarg, NULL, 0);
 			break;
-		  case 'd':
+		  case 'n': /* ADC input voltage range */
+			opt_range = !!strtoul(optarg, NULL, 0);
+			break;
+		  case 'o': /* ADC oversampling rate */
+			opt_oversample = (strtoul(optarg, NULL, 0) & 0x7);
+			break;
+		  case 'd': /* DAC channel number */
 			opt_setdac = strtoul(optarg, NULL, 0);
 			break;
-		  case 'v':
+		  case 'v': /* DAC millivolts */
 			opt_mvolts = strtoul(optarg, NULL, 0);
 			if (opt_mvolts < 0) opt_mvolts = 0;
 			if (opt_mvolts > 10000) opt_mvolts = 10000;
 			break;
-		  case 'p':
+		  case 'p': /* PWM Channel */
 			opt_pwm = strtoul(optarg, NULL, 0);
 			break;
-		  case 'u':
+		  case 'u': /* PWM duty cycle */
 			opt_pwmduty = strtoul(optarg, NULL, 0);
 			if (opt_pwmduty < 0) opt_pwmduty = -1;
 			if (opt_pwmduty > 1000) opt_pwmduty = 1000;
 			break;
-		  case 'P':
+		  case 'P': /* PWM/HBridge prescaler */
 			opt_prescaler = strtoul(optarg, NULL, 0);
 			break;
-		  case '1':
+		  case '1': /* HBridge affect the first channel */
 			opt_hb = 1;
 			break;
-		  case '2':
+		  case '2': /* HBridge affect the second channel */
 			opt_hb = 2;
 			break;
-		  case 'I':
+		  case 'I': /* HBridge Disable/Coast */
 		  case 'C':
 			opt_hbset = disable;
 			break;
-		  case 'B':
+		  case 'B': /* HBridge brake */
 			opt_hbset = brake;
 			break;
-		  case 'F':
+		  case 'F': /* HBridge forward duty cycle */
 			opt_hbset = fwd;
 			opt_hbduty = strtoul(optarg, NULL, 0);
 			if (opt_hbduty < 0) opt_hbduty = 0;
 			if (opt_hbduty > 1000) opt_hbduty = 1000;
 			break;
-		  case 'E':
+		  case 'E': /* HBridge reverse duty cycle */
 			opt_hbset = rev;
 			opt_hbduty = strtoul(optarg, NULL, 0);
 			if (opt_hbduty < 0) opt_hbduty = 0;
 			if (opt_hbduty > 1000) opt_hbduty = 1000;
 			break;
-		  case 'D':
+		  case 'D': /* IO set output */
 			opt_DO = 1;
 			opt_DOarg = strtoul(optarg, NULL, 0);
 			break;
-		  case 'G':
+		  case 'G': /* IO get input */
 			opt_DI = 1;
 			break;
-		  case 'c':
+		  case 'c': /* IO print counter */
 			opt_counter = 1;
 			opt_counterarg = strtoul(optarg, NULL, 0);
 			break;
-		  case 'R':
+		  case 'R': /* Gen. read from FPGA reg */
 			opt_read = 1;
 			break;
-		  case 'W':
+		  case 'W': /* Gen. write data to FPGA reg */
 			opt_write = 1;
 			opt_writearg = strtoul(optarg, NULL, 0);
 			break;
-		  case 'A':
+		  case 'A': /* Gen. Address to read/write */
 			opt_address = strtoul(optarg, NULL, 0);
 			break;
 		  case 'h':
@@ -253,6 +284,8 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
+
+
 	if (opt_DI) printf("dio=0x%x\n", ts8820_di_get());
 
 	if (opt_counter) {
@@ -264,9 +297,34 @@ int main(int argc, char **argv) {
 
 	if (opt_DO) ts8820_do_set(opt_DOarg);
 
-	if (opt_sample) ts8820_adc_sam(opt_rate, opt_sample);
+	if (opt_sample || opt_acquire) {
+		/* TODO: Look in to the currently undocumented libgpiod API for
+		 * better information on bulk/group setting of GPIO pins
+		 */
+		chip = gpiod_chip_open_by_number(3);
+		line22_range = gpiod_chip_get_line(chip, 22);
+		line23_range = gpiod_chip_get_line(chip, 23);
+		gpiod_line_request_output(line22_range, "ts8820ctl", opt_range);
+		gpiod_line_request_output(line23_range, "ts8820ctl", opt_range);
 
-	if (opt_acquire) ts8820_adc_acq(opt_rate, opt_acquire, opt_mask);
+		if (opt_oversample) {
+			line24_os = gpiod_chip_get_line(chip, 24);
+			line25_os = gpiod_chip_get_line(chip, 25);
+			line26_os = gpiod_chip_get_line(chip, 26);
+			gpiod_line_request_output(line24_os, "ts8820ctl",
+			  !!(opt_oversample & 0x1));
+			gpiod_line_request_output(line25_os, "ts8820ctl",
+			  !!(opt_oversample & 0x2));
+			gpiod_line_request_output(line26_os, "ts8820ctl",
+			  !!(opt_oversample & 0x4));
+		}
+
+		if (opt_sample) ts8820_adc_sam(opt_rate, opt_sample, opt_range);
+
+		if (opt_acquire) ts8820_adc_acq(opt_rate, opt_acquire, opt_mask);
+	}
+
+
 
 	if (opt_setdac) {
 		if (opt_setdac > 0 && opt_setdac <= 4) {
