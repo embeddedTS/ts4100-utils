99ec61083220efae3ff8fd0ba388e722c5d7e31f
Kris Bahnsen <kris@embeddedTS.com>
2024-10-17 12:34:02 -0700

    src: load_fpga: convert to libgpiod
    Signed-off-by: Kris Bahnsen <kris@embeddedTS.com>

--- src/ispvm.h
+++ src/ispvm.h
@@ -1,15 +1,10 @@
 #ifndef _ISPVM_H_
 #define _ISPVM_H_
 
-static int tmsfd = -1;
-static int tckfd = -1;
-static int tdofd = -1;
-static int tdifd = -1;
-
-#define g_ucPinTDI 		0x1
-#define g_ucPinTCK 		0x2
-#define g_ucPinTMS 		0x4
-#define g_ucPinTDO 		0x8
+#define g_ucPinTDI 	0x1
+#define g_ucPinTCK 	0x2
+#define g_ucPinTMS 	0x4
+#define g_ucPinTDO 	0x8
 #define g_ucPinENABLE 	0x10
 #define g_ucPinTRST 	0x20
 
--- src/load_fpga-ts4100.c
+++ src/load_fpga-ts4100.c
@@ -3,87 +3,90 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <gpiod.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-#include "gpiolib.h"
 #include "ispvm.h"
 
-#define TS4100_JTAG_TMS 134
-#define TS4100_JTAG_TCK 135
-#define TS4100_JTAG_TDO 132
-#define TS4100_JTAG_TDI 133
-#define TS4100_FPGA_RESETN 107
+static struct gpiod_chip *chip4;
+static struct gpiod_chip *chip3;
+static struct gpiod_line *jtag_tms;
+static struct gpiod_line *jtag_tck;
+static struct gpiod_line *jtag_tdo;
+static struct gpiod_line *jtag_tdi;
+static struct gpiod_line *fpga_resetn;
+
+#define TS4100_JTAG_TMS_LINE	6 // Chip 4
+#define TS4100_JTAG_TCK_LINE	7 // Chip 4
+#define TS4100_JTAG_TDO_LINE	4 // Chip 4
+#define TS4100_JTAG_TDI_LINE	5 // Chip 4
+#define TS4100_FPGA_RESETN_LINE 11 // Chip 3
 
 void reset_ts4100(void)
 {
-	assert(gpio_export(TS4100_FPGA_RESETN) == 0);
-
-	gpio_direction(TS4100_FPGA_RESETN, 1);
+	gpiod_line_set_value(fpga_resetn, 0);
 	/* Give 10ms of reset */
 	usleep(1000*10);
-	gpio_direction(TS4100_FPGA_RESETN, 2);
-	gpio_unexport(TS4100_FPGA_RESETN);
+	gpiod_line_set_value(fpga_resetn, 1);
 }
 
 void init_ts4100(void)
 {
-	assert(gpio_export(TS4100_JTAG_TMS) == 0);
-	assert(gpio_export(TS4100_JTAG_TCK) == 0);
-	assert(gpio_export(TS4100_JTAG_TDO) == 0);
-	assert(gpio_export(TS4100_JTAG_TDI) == 0);
+	chip3 = gpiod_chip_open_by_number(3);
+	chip4 = gpiod_chip_open_by_number(4);
 
-	tmsfd = gpio_getfd(TS4100_JTAG_TMS);
-	tckfd = gpio_getfd(TS4100_JTAG_TCK);
-	tdofd = gpio_getfd(TS4100_JTAG_TDO);
-	tdifd = gpio_getfd(TS4100_JTAG_TDI);
+	assert(chip3 != NULL);
+	assert(chip4 != NULL);
 
-	assert(tmsfd >= 0);
-	assert(tckfd >= 0);
-	assert(tdofd >= 0);
-	assert(tdifd >= 0);
+	jtag_tms = gpiod_chip_get_line(chip4, TS4100_JTAG_TMS_LINE);
+	jtag_tck = gpiod_chip_get_line(chip4, TS4100_JTAG_TCK_LINE);
+	jtag_tdo = gpiod_chip_get_line(chip4, TS4100_JTAG_TDO_LINE);
+	jtag_tdi = gpiod_chip_get_line(chip4, TS4100_JTAG_TDI_LINE);
+	fpga_resetn = gpiod_chip_get_line(chip3, TS4100_FPGA_RESETN_LINE);
 
-	gpio_direction(TS4100_JTAG_TCK, 2);
-	gpio_direction(TS4100_JTAG_TDI, 2);
-	gpio_direction(TS4100_JTAG_TMS, 2);
-	gpio_direction(TS4100_JTAG_TDO, 0);
+	assert(jtag_tms != NULL);
+	assert(jtag_tck != NULL);
+	assert(jtag_tdo != NULL);
+	assert(jtag_tdi != NULL);
+
+	assert(gpiod_line_request_output(jtag_tms, "load_fpga", 1) == 0);
+	assert(gpiod_line_request_output(jtag_tck, "load_fpga", 1) == 0);
+	assert(gpiod_line_request_output(jtag_tdi, "load_fpga", 1) == 0);
+	assert(gpiod_line_request_output(fpga_resetn, "load_fpga", 1) == 0);
+	assert(gpiod_line_request_input(jtag_tdo, "load_fpga") == 0);
 }
 
 void restore_ts4100(void)
 {
-	gpio_unexport(TS4100_JTAG_TMS);
-	gpio_unexport(TS4100_JTAG_TDI);
-	gpio_unexport(TS4100_JTAG_TCK);
-	gpio_unexport(TS4100_JTAG_TDO);
+	gpiod_line_release(jtag_tms);
+	gpiod_line_release(jtag_tdi);
+	gpiod_line_release(jtag_tck);
+	gpiod_line_release(jtag_tdo);
+	gpiod_line_release(fpga_resetn);
+
+	gpiod_chip_close(chip3);
+	gpiod_chip_close(chip4);
 }
 
 int readport_ts4100(void)
 {
-	//return gpio_read(TS4100_JTAG_TDO);
-	char in;
-	read(tdofd, &in, 1);
-	lseek(tdofd, 0, SEEK_SET);
-	if(in == '1') return 1;
-	return 0;
+	return gpiod_line_get_value(jtag_tdo);
 }
 
 void writeport_ts4100(int pins, int val)
 {
-	uint8_t *buf;
-	if(val) buf = "1";
-	else buf = "0";
-
 	switch (pins) {
 	case g_ucPinTDI:
-		write(tdifd, buf, 1);
+		gpiod_line_set_value(jtag_tdi, val);
 		break;
 	case g_ucPinTCK:
-		write(tckfd, buf, 1);
+		gpiod_line_set_value(jtag_tck, val);
 		break;
 	case g_ucPinTMS:
-		write(tmsfd, buf, 1);
+		gpiod_line_set_value(jtag_tms, val);
 		break;
 	default:
 		printf("%s: requested unknown pin\n", __func__);
@@ -92,6 +95,6 @@ void writeport_ts4100(int pins, int val)
 
 void sclock_ts4100()
 {
-	write(tckfd, "1", 1);
-	write(tckfd, "0", 1);
+	writeport_ts4100(g_ucPinTCK, 1);
+	writeport_ts4100(g_ucPinTCK, 0);
 }
